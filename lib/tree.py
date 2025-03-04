4dee6f538ded5b14958ecdff61db151477075541
Nili Scharf <nilischarf@gmail.com>
2025-03-04 11:51:14 +0200

    complete dsa tree transversal

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,20 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    stack = [self.root]
+    while stack:
+      node = stack.pop()
+      if node.get('id') == id:
+        return node
+      stack.extend(reversed(node.get('children', [])))
+    return None
+  
+  def get_element_by_id_bfs(self, id):
+    from collections import deque
+    queue = deque([self.root])
+    while queue:
+      node = queue.popleft()
+      if node.get('id') == id:
+        return node
+      queue.extend(node.get('children', []))
+    return None
